f4da3b1da2eee30c13aead271f031882944c80b1
Frances <n86903s@google.com>
2022-09-02 10:40:08 +0800

    Fix cards reducer

--- src/actions/cardsActions.ts
+++ src/actions/cardsActions.ts
@@ -2,6 +2,9 @@ import { PlantCard } from "../types/plantCardType";
 export enum CardsActions {
   ADD_NEW_PLANT_CARD,
   DELETE_PLANT_CARD,
+  EDIT_PLANT_NAME,
+  EDIT_PLANT_OWNER,
+  EDIT_PLANT_TAGS,
 }
 interface addNewPlantCard {
   type: CardsActions.ADD_NEW_PLANT_CARD;
@@ -15,4 +18,30 @@ interface deletePlantCard {
     cardId: string;
   };
 }
-export type CardsActionTypes = addNewPlantCard | deletePlantCard;
+interface editPlantName {
+  type: CardsActions.EDIT_PLANT_NAME;
+  payload: {
+    cardId: string;
+    plantName: string;
+  };
+}
+interface editPlantOwner {
+  type: CardsActions.EDIT_PLANT_OWNER;
+  payload: {
+    cardId: string;
+    ownerId: string;
+  };
+}
+interface editPlantTags {
+  type: CardsActions.EDIT_PLANT_TAGS;
+  payload: {
+    cardId: string;
+    tags: string[];
+  };
+}
+export type CardsActionTypes =
+  | addNewPlantCard
+  | deletePlantCard
+  | editPlantName
+  | editPlantOwner
+  | editPlantTags;
--- src/reducer/cardsReducer.ts
+++ src/reducer/cardsReducer.ts
@@ -6,16 +6,40 @@ const initialCards: PlantCard[] = [];
 const cards = (state = initialCards, action: CardsActionTypes) => {
   switch (action.type) {
     case CardsActions.ADD_NEW_PLANT_CARD: {
-      let currentCards = [...state];
+      let currentCards = JSON.parse(JSON.stringify(state));
       currentCards.push(action.payload.newCard);
       return currentCards;
     }
     case CardsActions.DELETE_PLANT_CARD: {
-      let currentCards = [...state];
+      let currentCards = JSON.parse(JSON.stringify(state));
       let deleteCardIndex = currentCards.findIndex(
-        (card) => card.cardId === action.payload.cardId
+        (card: PlantCard) => card.cardId === action.payload.cardId
       );
       currentCards.splice(deleteCardIndex, 1);
+      return currentCards;
+    }
+    case CardsActions.EDIT_PLANT_NAME: {
+      let currentCards = JSON.parse(JSON.stringify(state));
+      let editCardIndex = currentCards.findIndex(
+        (card: PlantCard) => card.cardId === action.payload.cardId
+      );
+      currentCards[editCardIndex].plantName = action.payload.plantName;
+      return currentCards;
+    }
+    case CardsActions.EDIT_PLANT_TAGS: {
+      let currentCards = JSON.parse(JSON.stringify(state));
+      let editCardIndex = currentCards.findIndex(
+        (card: PlantCard) => card.cardId === action.payload.cardId
+      );
+      currentCards[editCardIndex].tags.concat(action.payload.tags);
+      return currentCards;
+    }
+    case CardsActions.EDIT_PLANT_OWNER: {
+      let currentCards = JSON.parse(JSON.stringify(state));
+      let editCardIndex = currentCards.findIndex(
+        (card: PlantCard) => card.cardId === action.payload.cardId
+      );
+      currentCards[editCardIndex].ownerId = action.payload.cardId;
       return currentCards;
     }
   }
