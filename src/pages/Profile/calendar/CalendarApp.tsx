aa21bf36599d2a937f3cb7fb3ae4143247e8de5d
Frances <n86903s@google.com>
2022-09-06 17:28:06 +0800

    modify DB format: only save card Id to db, and get card data by Id

--- src/pages/Profile/calendar/CalendarApp.tsx
+++ src/pages/Profile/calendar/CalendarApp.tsx
@@ -3,8 +3,17 @@ import styled from "styled-components";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { unixTimeToString } from "../cards/CardEditor";
-import { db } from "../../../utils/firebase";
-import { doc, getDoc, collection } from "firebase/firestore";
+import { db, cards } from "../../../utils/firebase";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  collection,
+  where,
+  query,
+  DocumentData,
+} from "firebase/firestore";
+import { PlantCard } from "../../../types/plantCardType";
 
 const CalendarApp = () => {
   let defaultState = {
@@ -16,12 +25,36 @@ const CalendarApp = () => {
 
   useEffect(() => {
     async function getEventData() {
+      let eventRef;
       let docName = unixTimeToString(value.getTime());
       const activitiesRef = collection(db, "users", "test", "activities");
       let docRef = doc(activitiesRef, docName);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
-        setEvents(docSnap.data());
+        eventRef = docSnap.data();
+        let cardList: DocumentData[] = [];
+        let waterEvents = docSnap.data().watering;
+        let fertilizeEvents = docSnap.data().fertilizing;
+        let eventIds = waterEvents.concat(
+          fertilizeEvents.filter(
+            (item: string) => waterEvents.indexOf(item) < 0
+          )
+        );
+        const q = query(cards, where("cardId", "in", eventIds));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          cardList.push(doc.data());
+        });
+        let waterRef = eventRef.watering.map((id: string) => {
+          return cardList.find((card) => card.cardId === id)?.plantName;
+        });
+        let fertilizeRef = eventRef.fertilizing.map((id: string) => {
+          return cardList.find((card) => card.cardId === id)?.plantName;
+        });
+        setEvents({
+          watering: waterRef,
+          fertilizing: fertilizeRef,
+        });
       } else setEvents(defaultState);
     }
     getEventData();
--- src/pages/Profile/cards/CardsGrid.tsx
+++ src/pages/Profile/cards/CardsGrid.tsx
@@ -162,7 +162,7 @@ const CardsGrid = () => {
     setCheckList(newObj);
     event?.stopPropagation();
   }
-  async function addEventToDB(type: "water" | "fertilize", plants: string[]) {
+  async function addEventToDB(type: "water" | "fertilize", plantIds: string[]) {
     let docName = unixTimeToString(Date.now());
     const activitiesRef = collection(db, "users", "test", "activities");
     let docRef = doc(activitiesRef, docName);
@@ -171,9 +171,9 @@ const CardsGrid = () => {
       await setDoc(docRef, { watering: [], fertilizing: [] });
     }
     if (type === "water") {
-      await updateDoc(docRef, { watering: arrayUnion(...plants) });
+      await updateDoc(docRef, { watering: arrayUnion(...plantIds) });
     } else if (type === "fertilize") {
-      await updateDoc(docRef, { fertilizing: arrayUnion(...plants) });
+      await updateDoc(docRef, { fertilizing: arrayUnion(...plantIds) });
     }
   }
   function addEvents(type: "water" | "fertilize") {
@@ -181,17 +181,19 @@ const CardsGrid = () => {
       (key) => checkList[key] === true
     );
     if (!eventIds.length) return;
+    let idList: string[] = [];
     let nameList: string[] = [];
     eventIds.forEach((eventId) => {
-      let targetCard = cardList.filter((card) => card.cardId === eventId)[0];
-      nameList.push(targetCard.plantName);
+      let targetCard = cardList.find((card) => card.cardId === eventId);
+      idList.push(targetCard!.cardId);
+      nameList.push(targetCard!.plantName);
     });
     if (type === "water") {
       alert(`已爲 ${nameList.join(" & ")} 澆水！`);
     }
     if (type === "fertilize") alert(`已爲 ${nameList.join(" & ")} 施肥！`);
     clearAllCheck();
-    addEventToDB(type, nameList);
+    addEventToDB(type, idList);
   }
   async function deleteCard(cardId: string) {
     dispatch({
